1eee997bbccd8e1505ae6b1db13550fb57dd2217
iyesogie <79468835+iyesogie@users.noreply.github.com>
2021-04-06 00:07:21 -0400

    pushing PyBank and PyPoll

---
+++ pyBank/main.py
@@ -0,0 +1,86 @@
+import os
+import csv
+
+
+budget_data_csv = os.path.join ("..","pyBank", "resources2", "budget_data.csv")
+
+Months_Amount = 0
+Profit = 0
+Months = []
+Total_Profit = 0
+Difference = 0
+Total_Difference = []
+
+
+
+with open(budget_data_csv)as csvfile:
+    csv_reader=csv.reader(csvfile, delimiter= ",")
+
+    csvheader=next(csv_reader)
+
+
+     #read through data after header
+    for row in csv_reader:
+       
+
+        Months_Amount += 1
+
+
+         
+        Total_Profit = Total_Profit + int(row[1])
+
+        Months.append(str(row[0]))
+        if Difference != 0:
+            profit = int(row[1])
+
+            Difference = profit -Difference
+
+            Total_Difference.append(Difference)
+
+            Difference = int(row[1])
+        elif Difference == 0:
+            Difference = int(row[1])
+    Months.pop(0)
+
+        
+
+    Greatest_Increase = Total_Difference.index(max(Total_Difference))
+    Greatest_Decrease = Total_Difference.index(min(Total_Difference))
+
+    # Find Months
+    Increase_Difference = (Months[int(Greatest_Increase)], max(Total_Difference))
+    Decrease_Difference = (Months[int(Greatest_Decrease)], min(Total_Difference))
+    
+
+    mean=sum(Total_Difference)/float(len(Total_Difference))  
+    mean=round(mean,2)
+
+
+    print(f'Financial Analysis')
+    print(f'-------------------------------------------')
+    print(f'Total Months: {Months_Amount}')
+    print(f'Net Profit: {Total_Profit}')
+    print(f'Average Monthly Change: {mean}')
+    print(f'Greatest Increase in Profits: {Increase_Difference}')
+    print(f'Greatest Loss In Profits: {Decrease_Difference}')
+
+    PyBanktxt =os.path.join('..','pyBank', 'PyBanktxt')
+
+    with open(PyBanktxt, 'w') as txtfile:
+        txtfile.write('Financial Analysis')
+        txtfile.write('\n------------------------------------')
+        txtfile.write(f'\nTotal Months: {Months_Amount}')
+        txtfile.write(f'\nNet Profit: {Total_Profit}')
+        txtfile.write(f'\nAverage Monthly Change: {mean}')
+        txtfile.write(f'\nGreatest Increase In Profits: {Increase_Difference}')
+        txtfile.write(f'\nGreatest Loss In Profits: {Decrease_Difference}')
+
+
+
+
+
+    
+
+    
+    
+      
